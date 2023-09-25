5abcf8d75040c9941a442763fe326d8c0b7fefce
Matt Naples <mattnaples50@gmail.com>
2023-09-25 09:44:02 -0400

    Initial Commit

---
+++ MovieFinder/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MovieFinder
+//
+//  Created by matt naples on 9/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieFinder/MovieFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFinderApp.swift
+//  MovieFinder
+//
+//  Created by matt naples on 9/25/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
